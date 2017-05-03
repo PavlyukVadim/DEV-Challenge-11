6305eace97c2d3ad87fd19f62eb41c7dfb30bd22
Vadim Pavlyuk <pavlyuk.dev@gmail.com>
2017-05-04 01:30:40 +0300

    basic Grid, Cell components

---
+++ src/components/Cell/Cell.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Cell.scss';
+
+class Cell extends Component {
+  render() {
+    return (
+      <div className="cell">
+        
+      </div>
+    );
+  }
+}
+
+export default Cell;
---
+++ src/components/Grid/Grid.jsx
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import Cell from '../Cell/Cell';
+import './Grid.scss';
+
+class Grid extends Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = {};
+    this.width = 50;
+    this.height = 50;
+    this.gridStyle = {
+      width: this.width * 14 + 'px',
+      height: this.height * 14 + 'px'
+    };
+  }
+
+  getCellGrid() {
+    let grid = [];
+    let i, j;
+    for (i = 0; i < this.width; i++) {
+      for (j = 0; j < this.height; j++) {
+        grid.push(<Cell key={i * this.width + j} />);
+      }
+    }
+    return grid;
+  }
+
+  render() {
+    let grid = this.getCellGrid();
+    return (
+      <div id="grid" style={this.gridStyle}>
+        {grid}
+      </div>
+    );
+  }
+}
+
+export default Grid;
