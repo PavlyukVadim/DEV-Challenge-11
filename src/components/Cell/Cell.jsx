ce46657afd5f33d57938eb673d467d312d79f775
Vadim Pavlyuk <pavlyuk.dev@gmail.com>
2017-05-04 01:51:52 +0300

    handler cell onClick

--- src/components/Cell/Cell.jsx
+++ src/components/Cell/Cell.jsx
@@ -2,11 +2,29 @@ import React, { Component } from 'react';
 import './Cell.scss';
 
 class Cell extends Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+			alive: this.props.alive
+    };
+    this.switchCellState = this.switchCellState.bind(this);
+  }
+
+  switchCellState() {
+		this.setState((prevState) => {
+  		return {
+  			alive: !prevState.alive
+  		};
+		});
+  }
+
   render() {
-  	let alive = this.props.alive;
+  	let alive = this.state.alive;
     return (
-      <div className={"cell " + (alive ? "alive" : "")}>
-        
+      <div className={"cell " + (alive ? "alive" : "")}
+           onClick={this.switchCellState}
+           ref={(input) => { this.textInput = input; }}>
       </div>
     );
   }
